9eb179735e8b48b887ea4f8e32743272f1d6ba97
jalpa1998 <162426649+jalpa1998@users.noreply.github.com>
2024-07-12 16:02:19 +0530

    Add files via upload

---
+++ MODUAL 4.0/sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double firstnumber,secondnumber;
+	
+	cout<<"enter the first number:"<<endl;
+	cin>>firstnumber;
+	
+	cout<<"enter the second number:"<<endl;
+	cin>>secondnumber;
+	
+	cout<<"firstnumber + secondnumber ="<<firstnumber + secondnumber<<endl;
+	
+	cout<<"firstnumber - secondnumber ="<<firstnumber - secondnumber<<endl;
+	
+	cout<<"firstnumber * secondnumber ="<<firstnumber * secondnumber<<endl;
+	
+	cout<<"firstnumber / secondnumber ="<<firstnumber / secondnumber<<endl;
+}
